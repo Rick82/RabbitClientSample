121196fe5ba810f56bb40676c5ed8a1b6a8bdbcc
Rick <Rick@DESKTOP-85ANAIC>
2024-06-07 00:48:03 +0800

    RabbitClient basic public/receive sample

---
+++ Receive/Receive.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Reflection;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost", DispatchConsumersAsync = true };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello", false, false, false, null);
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+
+consumer.Received += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Message Get" + ea.DeliveryTag);
+    Console.WriteLine($" [x] Received {message}");
+    await Task.Yield();
+};
+
+channel.BasicConsume(queue: "hello",
+    autoAck: true,
+    consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Send/Send.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{HostName = "localhost",DispatchConsumersAsync = true};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello", false, false, false, null);
+
+const string message = "Hello World!";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty,"hello",null,body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
+
